b05cb29b8f137cc033c524d3ba2e34a38ef4bc80
Vitaly898 <vetalpetrov2016@yandex.ru>
2020-10-31 00:45:39 +0300

    feat: new completed tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+  calculateDepth(arr) {
+    let count = 0;
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            count = Math.max(count, this.calculateDepth(arr[i]));
+        }
+        count++;
+    }
+    return count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
+
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,36 @@
 const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
   chain:[],
   getLength() {
-    return this.chain.length()
+    
+    return this.chain.length;
   },
   addLink(value) {
-    
+    if (value === undefined){
+      this.chain.push(`()`)}
+      else{this.chain.push(`( ${value} )`)
+    }
+    return this
+      
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  if(typeof(position) !== 'number' || position < 0 || position > this.getLength() || parseInt(position,10) !==position){
+    this.chain = []
+    throw new Error('wrong position')
+  }
+    else{
+   this.chain.splice(position-1,1)}
+   return this
   },
+  
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   result =  this.chain.join('~~')
+   this.chain = []
+    return result
   }
 };
 
